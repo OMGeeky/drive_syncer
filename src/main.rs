cafe5aac8656ba76c388ff00d92b1e135d354a28
OMGeeky <aalaalgmx@gmail.com>
2023-05-14 17:56:04 +0200

    rename to drive_syncer from untitled

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[tokio::main]
 async fn main() {
-    untitled::init_logger();
+    drive_syncer::init_logger();
     // use tokio::runtime::Runtime;
 
     // let rt = Runtime::new().unwrap();
@@ -8,12 +8,12 @@ async fn main() {
     //
     // let handle = rt.handle();
     // handle.block_on(async {
-    // untitled::sample().await.unwrap();
-    // untitled::sample_fs().await.unwrap();
-    // untitled::google_drive::sample().await.unwrap();
-    // untitled::watch_file_reading().await.unwrap();
-    // untitled::sample_nix().await.unwrap();
-    untitled::sample_drive_fs().await.unwrap();
+    // drive_syncer::sample().await.unwrap();
+    // drive_syncer::sample_fs().await.unwrap();
+    // drive_syncer::google_drive::sample().await.unwrap();
+    // drive_syncer::watch_file_reading().await.unwrap();
+    // drive_syncer::sample_nix().await.unwrap();
+    drive_syncer::sample_drive_fs().await.unwrap();
     // });
     // RUNTIME.block_on(async {
     //     //test
